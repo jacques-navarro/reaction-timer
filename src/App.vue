bfaf7a2cda31b1986a805d0d23074ac2d801d6ee
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2024-07-21 17:25:26 +0200

    Add Block component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <h1>Reaction Timer</h1>
-  <button @click="start">Play</button>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay"/>
 </template>
 
 <script>
+import Block from "./components/Block.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: { Block },
 
   data() {
     return {
@@ -17,9 +20,9 @@ export default {
 
   methods: {
     start() {
-      this.delay = Math.floor(Math.random() * 5000) + 2000
-      this.isPlaying = true
-      console.log(`Delay: ${this.delay}`)
+      this.delay = Math.floor(Math.random() * 5000) + 2000;
+      this.isPlaying = true;
+      console.log(`Delay: ${this.delay}`);
     },
   },
 };
