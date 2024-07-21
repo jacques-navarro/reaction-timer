8e63eb3b89ebda979ba5b563413e13da1d3b2aee
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2024-07-21 23:38:56 +0200

    Add showBlock property, mounted lifecycle hook

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,10 +1,22 @@
 <template>
-  <div class="block">Click me</div>
+  <div class="block" v-if="showBlock">Click me</div>
 </template>
 
 <script>
 export default {
   props: ["delay"],
+
+  data() {
+    return {
+      showBlock: false,
+    };
+  },
+
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true;
+    }, this.delay);
+  },
 };
 </script>
 
